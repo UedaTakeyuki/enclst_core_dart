3fddcfd22e470def57c0c6c70e45b4bda67968cb
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2024-11-03 23:27:51 +0900

    check if YT contents and playlist

--- lib/mixins/value_checker.dart
+++ lib/mixins/value_checker.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/foundation.dart';
+
 mixin ValueChecker {
+  // is this Path?
   bool isPath(String s) {
     if (s.substring(0, 6) == "http://" ||
         s.substring(0, 7) == "https://" ||
@@ -10,6 +13,45 @@ mixin ValueChecker {
       return true;
     } else {
       return false;
+    }
+  }
+
+  // is this Enclst?
+  bool isEnclst(String s) {
+    if (s.endsWith('.enclst')) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  // is this youtube contents ID?
+  bool isYTcontentID(String s) {
+    if (isPath(s) || isEnclst(s)) {
+      return false;
+    } else {
+      if (s.length == 11) {
+        return true;
+      } else {
+        return false;
+      }
+    }
+  }
+
+  // is this youtube playlist ID?
+  bool isYTplaylistID(String s) {
+    if (isPath(s) || isEnclst(s)) {
+      return false;
+    } else {
+      if (s.substring(0, 2) == "PL") {
+        if (s.length == 18 || s.length == 34) {
+          return true;
+        } else {
+          return false;
+        }
+      } else {
+        return false;
+      }
     }
   }
 }
--- test/value_checker_test.dart
+++ test/value_checker_test.dart
@@ -2,8 +2,30 @@ import 'package:enclst_core_dart/enclst_core_dart.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-  final enclst = Enclst("");
+  final e = Enclst("");
   test('test isPath', () {
-    expect(enclst.isPath("http://ahoaho"), true);
+    expect(e.isPath("http://ahoaho"), true);
+    expect(e.isPath("https://ahoaho"), true);
+    expect(e.isPath("ahoaho/boke"), true);
+    expect(e.isPath("./ahoaho/boke"), true);
+    expect(e.isPath("/ahoaho/boke"), true);
+    expect(e.isPath("ahoahoboke"), false);
+  });
+  test("test isEnclst", () {
+    expect(e.isEnclst("aho.enclst"), true);
+    expect(e.isEnclst('aho.enclist'), false);
+  });
+  test("test isYTcontentID", () {
+    expect(e.isYTcontentID("NiRIbKwAejk"), true);
+    expect(e.isYTcontentID("NiRIbKwAejk0"), false);
+    expect(e.isYTcontentID("NiRI.enclst"), false);
+    expect(e.isYTcontentID("https://NiR"), false);
+  });
+  test("is isYTplaylistID", () {
+    expect(e.isYTplaylistID("PLA51C5154D20F440C"), true);
+    expect(e.isYTplaylistID("PLQWzKIaERirwyaLCHXafULQs4In_FS2WA"), true);
+    expect(e.isYTplaylistID("PLQWzKIaERirwyaLCHXafULQs4In_FS2WA0"), false);
+    expect(e.isYTplaylistID("PLQWzKIaERirwyaLCHXafULQs4I.enclst"), false);
+    expect(e.isYTplaylistID("https://PLQWzKIaERirwyaLCHXafULQs4"), false);
   });
 }
