c34b07d7a5c34e1cb69127a8a1f144dcd01f2e9a
Dr. Takeyuki UEDA <ueda@latelierdueda.com>
2024-11-07 09:01:01 +0900

    remove redundant class

--- lib/enclst.dart
+++ lib/enclst.dart
@@ -1,4 +1,4 @@
-import 'package:enclst_core_dart/generating_file.dart';
+// import 'package:enclst_core_dart/generating_file.dart';
 import 'package:http/http.dart' as http;
 import 'package:enclst_core_dart/item.dart';
 import 'package:enclst_core_dart/mixins/value_checker.dart';
@@ -8,7 +8,8 @@ class Enclst with ValueChecker {
   String title = "";
   String url = "";
   List<Item> items = [];
-  GeneratingFile? g_file;
+//  GeneratingFile? g_file;
+  String? filepath;
 
   // Constructor
   Enclst(String enclstStr) {
@@ -43,13 +44,57 @@ class Enclst with ValueChecker {
     var response = await http.get(uri);
     var enclistStr = response.body;
     final enclst = Enclst(enclistStr);
-    enclst.g_file = GeneratingFile(urlStr);
+//    enclst.g_file = GeneratingFile(urlStr);
+    enclst.filepath = urlStr;
     return enclst;
   }
 
   // next Enclst
   nextEnclst(String path, {String v_root = ""}) async {
-    var next = g_file?.nextPath(path, v_root: v_root);
-    return await createFromURL(next);
+//    var next = g_file?.nextPath(path, v_root: v_root);
+    final nextfilepath = nextFilePath(path, v_root: v_root);
+    return await createFromURL(nextfilepath);
+  }
+
+  /**
+  Calculate subsequent URL from the current URL that storeed in the member valuable of this enclst, and a parameter path that indicate a next enclst which is loading.
+  Retuned nextURL is calculated as joind of the current url and path.
+  You can optionally specify a base_url, which will be used instead of the current url.
+
+  @param {string} currentURL Current URL.<br>
+  @param {string} path Next path.<br>
+  @param {string} base_url Base url of this path, or nil.<br>
+  @returns {string} created URL as follows:
+  <ul>
+    <li> if path is started from "http://", just return paht</li>
+    <li> else if path is started from "/"
+    <ul>
+      <li> if base_url is not specified, return CurrentURL + path
+      <li> if base_url is specified, return base_url + path
+    </ul>
+    <li> else, return URL(path, currentURL)
+  </ul>
+*/
+  nextFilePath(String path, {String v_root = ""}) {
+    if (isURL(path)) {
+      // path is full url
+      return path;
+    } else if (path[0] == "/") {
+      // URL - filename
+      String origin = (Uri.parse(filepath ?? "")).origin;
+      // path is full path
+      if (v_root != "") {
+        if (v_root[v_root.length - 1] == '/') {
+          // remove trailing '/'
+          v_root = v_root.substring(0, v_root.length - 1);
+        }
+        origin = v_root;
+      }
+      return origin + path;
+    } else {
+      // path is relative path
+      var context = p.Context(style: p.Style.url);
+      return (context.canonicalize(context.join(filepath ?? "", "..", path)));
+    }
   }
 }
--- test/nextURL_test.dart
+++ test/nextURL_test.dart
@@ -5,18 +5,18 @@ void main() async {
   final e = await Enclst.createFromURL(
       'https://raw.githubusercontent.com/UedaTakeyuki/HS2playlist/refs/heads/main/wagner/Theater/menu.enclst');
   test('test nextURL', () {
-    expect(e.g_file?.ganeratingFilePath,
+    expect(e.filepath,
         'https://raw.githubusercontent.com/UedaTakeyuki/HS2playlist/refs/heads/main/wagner/Theater/menu.enclst');
-    expect(e.g_file?.nextPath("Deutschland/menu.enclst"),
+    expect(e.nextFilePath("Deutschland/menu.enclst"),
         "https://raw.githubusercontent.com/UedaTakeyuki/HS2playlist/refs/heads/main/wagner/Theater/Deutschland/menu.enclst");
-    expect(e.g_file?.nextPath("/Deutschland/menu.enclst"),
+    expect(e.nextFilePath("/Deutschland/menu.enclst"),
         "https://raw.githubusercontent.com/Deutschland/menu.enclst");
     expect(
-        e.g_file?.nextPath("/Deutschland/menu.enclst",
+        e.nextFilePath("/Deutschland/menu.enclst",
             v_root: "https://raw.githubusercontent.com/UedaTakeyuki/"),
         "https://raw.githubusercontent.com/UedaTakeyuki/Deutschland/menu.enclst");
     expect(
-        e.g_file?.nextPath("/Deutschland/menu.enclst",
+        e.nextFilePath("/Deutschland/menu.enclst",
             v_root: "https://raw.githubusercontent.com/UedaTakeyuki"),
         "https://raw.githubusercontent.com/UedaTakeyuki/Deutschland/menu.enclst");
   });
